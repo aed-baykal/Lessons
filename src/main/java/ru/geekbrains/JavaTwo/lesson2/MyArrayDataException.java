7ea79f8f2237d473ba70cb4cf505eaf72dac9ac1
andrey <aedbaykal@gmail.com>
2021-04-25 02:23:14 +0300

    Д\З JAVA 2 Второй урок.

---
+++ src/main/java/ru/geekbrains/JavaTwo/lesson2/MyArrayDataException.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.JavaTwo.lesson2;
+
+public class MyArrayDataException extends NumberFormatException{
+
+    int i, j;
+    String string;
+
+    public MyArrayDataException(String string, int i, int j) {
+        this.i = i;
+        this.j = j;
+        this.string = string;
+    }
+
+    public void printMessage() {
+        System.out.printf("В ячейке массива strings[%d][%d] находится строка <<%s>> не являющаяся числом.\n", i, j, string);
+    }
+
+}

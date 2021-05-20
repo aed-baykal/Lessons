d8363367380d3f7e4e1de7350b9fd6a4ca664133
Andrey <aedbaykal@gmail.com>
2021-05-20 12:30:39 +0300

    Д/З к восьмому уроку

--- src/main/java/ru/geekbrains/java_two/april_chat/server/ClientHandler.java
+++ src/main/java/ru/geekbrains/java_two/april_chat/server/ClientHandler.java
@@ -7,6 +7,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClientHandler {
 
@@ -15,6 +18,7 @@ public class ClientHandler {
     private DataOutputStream outputStream;
     private DataInputStream inputStream;
     private String currentUsername;
+    private static final long TIME_OUT = 120000;
 
     public ClientHandler(Socket socket, ChatServer chatServer) {
         try {
@@ -23,6 +27,7 @@ public class ClientHandler {
             this.inputStream = new DataInputStream(socket.getInputStream());
             this.outputStream = new DataOutputStream(socket.getOutputStream());
             System.out.println("Client handler created!!!");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -31,8 +36,8 @@ public class ClientHandler {
     public void handle() {
         new Thread(() -> {
             try {
-                    authenticate();
-                    readMessages();
+                authenticate();
+                readMessages();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -73,9 +78,21 @@ public class ClientHandler {
         return this.currentUsername;
     }
 
-    private void authenticate() {
+    private void authenticate() throws SocketException {
         System.out.println("Started client  auth...");
-
+        TimerTask task = new TimerTask(){
+            @Override
+            public void run() {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Connection is closed!");
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, TIME_OUT);
         try {
             while (true) {
                 String authMessage = inputStream.readUTF();
@@ -83,7 +100,6 @@ public class ClientHandler {
                 ChatMessage msg = ChatMessage.unmarshall(authMessage);
                 String username = chatServer.getAuthService().getUsernameByLoginAndPassword(msg.getLogin(), msg.getPassword());
                 ChatMessage response = new ChatMessage();
-
                 if (username == null) {
                     response.setMessageType(MessageType.ERROR);
                     response.setBody("Wrong username or password!");
@@ -98,6 +114,7 @@ public class ClientHandler {
                     currentUsername = username;
                     chatServer.subscribe(this);
                     System.out.println("Subscribed");
+                    timer.cancel();
                     sendMessage(response);
                     break;
                 }
